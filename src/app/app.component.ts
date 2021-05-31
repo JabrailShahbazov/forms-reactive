8d369f81b16cfcd982231ecfdee689d7930d993e
Jabrail Shahbazov <cebrail.shahbazov@gmail.com>
2021-05-31 10:40:16 +0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  singupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.singupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.singupForm);
+  }
+
+  get controls() {
+    return (this.singupForm.get('hobbies') as FormArray).controls;
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.singupForm.get('hobbies')).push(control);
+  }
+}
